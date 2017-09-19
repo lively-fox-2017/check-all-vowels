87b9ccc73cf901869420a8608621ac96221a0e0d
albertagung <albiardtya@gmail.com>
2017-09-19 15:46:40 +0700

    albertagung

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,13 @@
-//write your code here
+function vowels(str){
+    var vows = ["a","i","u","e","o"]
+    for(var i = 0; i < str.length; i++){
+        console.log(vows.indexOf(str[i]))
+        if(vows.indexOf(str[i]) === -1){
+            return false
+        }
+    }
+    return true
+}
+console.log(vowels("aiueo"));
+console.log(vowels("wwwwww"));
+
