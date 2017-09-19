3a6d047842367921adfb5d96d8ba2d143d9b7b59
kalibani <kalibani.ka@gmail.com>
2017-09-19 16:34:33 +0700

    tugas check vowell done

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,24 @@
-//write your code here
+//release 0
+
+function checkAllVowel(str){
+	let vowel = ['a','i','u','e','o'];
+  for (var i = 0; i < str.length; i++) {
+    if (vowel.indexOf(str[i]) == -1) {
+     return false
+ 	 }
+  }
+return true
+}
+console.log(checkAllVowel('aqrst'))
+console.log(checkAllVowel('ooooo'))
+
+//release 1
+function stringToArray(str){
+	let strN=str.split(',');
+	let tampung=[];
+	for(i=0; i<strN.length; i++){
+		tampung.push(strN[i].split(''))
+	}
+return tampung
+}
+console.log(stringToArray('aqrst,ukaei,ffooo'))
