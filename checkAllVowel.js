9b548f28c67628435117b785ccdd8823b3cb5062
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-09-19 11:49:54 +0700

    create isAllVowels() to check if a string is all vowels

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,12 @@
-//write your code here
+function isAllVowels(string) {
+	const vowels = ['a', 'i', 'u', 'e', 'o'];
+	for (let i = 0; i < string.length; i++) {
+		let isConsonant = vowels.indexOf(string[i].toLowerCase()) === -1;
+		if (isConsonant) return false;
+	}
+	
+	return true;
+}
+
+console.log(isAllVowels('AaaIuEEo')); //true
+console.log(isAllVowels('AaMaIuEEo')); //false
