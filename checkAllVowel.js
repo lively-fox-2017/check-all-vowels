74a4e3ae05c5d9aa7455acad7491801c66055dfa
NNcrawler <rasyid.xyz@gmail.com>
2017-09-19 11:33:56 +0700

    fungsi cek string isi vokal semua

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,16 @@
 //write your code here
+/*buat fungsi untuk mengecek apakah suatu string terdiri dari huruf vokal semua atau tidak
+return true bila ditemukan
+return false bila tidak*/
+'use strict'
+function isAllVocal(str){
+  let arrStr = str.split('');
+  let vocals = 'aiueo';
+  for (let i of arrStr) {
+    if (vocals.indexOf(i.toLowerCase())==-1) return false;
+  }
+  return true;
+}
+
+console.log(isAllVocal('aiueo'));
+console.log(isAllVocal('aku '));
