51f1786a28964fb7fbe14aae9ce213a85347b415
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-19 11:50:23 +0700

    check vowel selesai

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,16 @@
 //write your code here
+
+
+let checkVowel = str=> {
+	const vowels = ['a', 'e', 'i', 'o', 'u'];
+	let newstr = str.toLowerCase();
+	for (i = 0; i < newstr.length; i++) {
+		if (vowels.indexOf(newstr[i]) ===  -1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(checkVowel('ab'))
+console.log(checkVowel('aeaiaoau'))
