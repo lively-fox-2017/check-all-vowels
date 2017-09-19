0be45fe603651d2be8ba85355f0f2dd786aa3e3c
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-19 11:42:40 +0700

    Commit checkvowels

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,15 @@
-//write your code here
+let checkAllVowels = (str) => {
+  let vowels = ['a', 'i', 'u', 'e', 'o'];
+  let arr = str.split("");
+  for (var i = 0; i < arr.length; i++) {
+    if (vowels.indexOf(arr[i]) === -1)
+      return false;
+  }
+  return true;
+}
+
+
+console.log(checkAllVowels('aaaaaaaaaaaaiiiiiiiiiiiiiiiisuuuuuuuuuuuuueeeeeeeeooooooo'));
+console.log(checkAllVowels('aaaa'));
+console.log(checkAllVowels('aaaaaaaaaaaaas'));
+console.log(checkAllVowels('siiiiiiiiiii'));
