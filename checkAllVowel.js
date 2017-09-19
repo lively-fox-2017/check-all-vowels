6d301bbd0555e890e5927c65933a962a8685ea19
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-19 10:54:28 +0700

    Check Vowels Done

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,16 @@
-//write your code here
+function checkvowel(str) {
+  var vowelArr = ["a","e","i","o","u"];
+  var total = 0;
+
+  for (var i = 0; i < vowelArr.length; i++) {
+    for (var j = 0; j < str.length; j++) {
+        if (str[j].toLowerCase() === vowelArr[i]) {
+            total++
+        }
+    }
+  }
+  return total === str.length;
+
+}
+console.log(checkvowel("aaaaaUaaaaaa"))
+console.log(checkvowel("aaaaakjsdbaskdisdaaaaaa"))
