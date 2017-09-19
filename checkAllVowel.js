209488922a7d1f88ca4775015e6d199457167fc3
ipunganwar <ipung.anwar1@gmail.com>
2017-09-19 15:33:06 +0700

    release 0

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,15 @@
 //write your code here
+function checkVowel(str){
+	var vowel = 'aiueo';
+
+	for(var i = 0; i < str.length; i++){
+		for(var j = 0; j < vowel.length; j++){
+			if(str[i] === vowel[j]){
+				return true;
+			}
+		}
+	}
+
+}
+
+console.log(checkVowel('jajaja'))
