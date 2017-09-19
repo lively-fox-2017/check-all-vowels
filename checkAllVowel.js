99e6c7d7ec9b8fdfd6124779c3d012ad4f6a0015
terrathe2 <putra.redha@gmail.com>
2017-09-19 11:29:29 +0700

    release 0 - Selasa

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,15 @@
 //write your code here
+function checkVowels(str){
+	let vowels = ["a", "i", "u", "e", "o"];
+	for(var i=0;i<=str.length-1;i++){
+		if(vowels.indexOf(str[i]) === -1){
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+console.log(checkVowels("redhaputra")); // false
+console.log(checkVowels("aiueo")); // true
+console.log(checkVowels("aaaaaaaa")); // true
