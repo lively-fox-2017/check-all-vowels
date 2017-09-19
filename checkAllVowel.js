61db277454f3f2408f3fe05e0768142ebf4a9b59
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-09-19 14:34:26 +0700

    Release 0

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,18 @@
 //write your code here
+function cekVowels(str){
+	const lib=['a','i','u','e','o']
+
+	let temp=str.split('')
+
+	for(let i=0;i<temp.length;i++){
+		for(let j=0;j<lib.length;j++){
+			if(temp[i]==lib[j]){
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+console.log(cekVowels('nizar'));
