1c8bbe832028ea2ae71d5ba3edc0d08511a62b18
aditReza <lete.note3@gmail.com>
2017-09-19 15:29:25 +0700

    release 0 : add cekVokal

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,16 @@
 //write your code here
+
+let vokal = 'aiueo';
+let hurufVokal = vokal.split('');
+
+function cekVokal(str) {
+    if (str == vokal) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(cekVokal('aqrst'));
+console.log(cekVokal('aiueo'));
+console.log(cekVokal('bjhabsc'));
