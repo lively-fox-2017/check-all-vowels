9b710239b301d1d782ccaa7a0faac3ef57a8f6b8
ameliarahman <amel.rahman5@gmail.com>
2017-09-19 14:07:00 +0700

    Release 0 done

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,16 @@
-//write your code here
+function checkAllVowel(arr){
+    var count =0;
+    for(var i=0; i< arr.length; i++){
+        for(var j=0; j<arr.length; j++){
+        if(arr[i][j] === 'A' || arr[i][j] === 'I' || arr[i][j] === 'U' || arr[i][j] === 'E' || arr[i][j] === 'O'){
+            count++;
+        }else{
+            return false;
+        }
+        }
+
+    }
+    return true;
+}
+
+console.log(checkAllVowel([['A', 'A', 'O', 'O', 'O'],['A', 'A', 'O', 'O', 'O'],['A', 'A', 'O', 'O', 'O']]));
