af736dcea647e2b02d7a7d4aa7547778f1121253
chandrabuwana <cbuwana@gmail.com>
2017-09-19 14:51:16 +0700

    checkAllvowels Done

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,1 +1,17 @@
 //write your code here
+function checkVowel(str){
+  let vowel=['a','i','u','e','o']
+  let low=str.toLowerCase()
+
+  for(let i=0;i<str.length;i++){
+    for(let j=0;j<str.length;j++){
+      if(low[j]===vowel[i]){
+        return true
+      }else{
+        return false
+      }
+    }
+  }
+}
+
+console.log(checkVowel('uop'));
