43c4f4329f5455f5e9f042592a8b9df65df3dc75
aditReza <lete.note3@gmail.com>
2017-09-22 02:18:34 +0700

    release 1

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -4,13 +4,36 @@ function cekVokal(str) {
     let counter = 0
 
     for (let i = 0; i < vokal.length; i++) {
-        if (str.indexOf(vokal[i] !== -1)) {
+        if (str.indexOf(vokal[i]) !== -1) {
             counter++
         }
     }
-    return (counter > 0)
+    console.log(counter);
+    return (counter > 0) ? true : false
 }
 
-// console.log(cekVokal('xxxx'));
+console.log(cekVokal('xxxx'));
 console.log(cekVokal('aiueo'));
-// console.log(cekVokal('bjhabsc'));
+console.log(cekVokal('bjhabsc'));
+
+/*
+array 2 dimensi
+*/
+
+function arrTwoDim(row, col) {
+    let input = 'aqrst, ukaei, ffooo'.split(',')
+    console.log(input)
+    let hasil = []
+
+    for (r = 0; r < row; r++) {
+        let row = []
+        for (c = 0; c < col; c++) {
+            row.push(input[r][c])
+        }
+        hasil.push(row)
+    }
+    return hasil
+}
+
+//arrTwoDim(5, 3);
+console.log(arrTwoDim(3, 5));
