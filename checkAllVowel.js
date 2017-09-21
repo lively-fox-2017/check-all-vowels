4b4a6df87a413b514dcf6750845a02c4010a595f
aditReza <lete.note3@gmail.com>
2017-09-22 02:04:37 +0700

    change function

--- checkAllVowel.js
+++ checkAllVowel.js
@@ -1,16 +1,16 @@
 //write your code here
-
-let vokal = 'aiueo';
-let hurufVokal = vokal.split('');
-
 function cekVokal(str) {
-    if (str == vokal) {
-        return true
-    } else {
-        return false
+    let vokal = 'aiueo'.split('')
+    let counter = 0
+
+    for (let i = 0; i < vokal.length; i++) {
+        if (str.indexOf(vokal[i] !== -1)) {
+            counter++
+        }
     }
+    return (counter > 0)
 }
 
-console.log(cekVokal('aqrst'));
+// console.log(cekVokal('xxxx'));
 console.log(cekVokal('aiueo'));
-console.log(cekVokal('bjhabsc'));
+// console.log(cekVokal('bjhabsc'));
